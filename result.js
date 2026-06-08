09485ca3b48160b1999685fb85a5158674e8cb1a
SEFAT AL NAHIAN <sefatalnahian@gmail.com>
2026-06-08 18:05:37 +0600

    fourth task completed

---
+++ result.js
@@ -0,0 +1,13 @@
+let marks = 75;
+
+if (marks >= 80) {
+  console.log('Your Grade: A+');
+} else if (marks <= 79 && marks >= 70) {
+  console.log('Your Grade: A');
+} else if (marks <= 69 && marks >= 60) {
+  console.log('Your Grade: B');
+} else if (marks <= 59 && marks >= 50) {
+  console.log('Your Grade: C');
+} else {
+  console.log('Fail');
+}
