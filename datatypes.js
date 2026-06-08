710f4fe6533cd101ef2da82c70e143146dc69f0f
SEFAT AL NAHIAN <sefatalnahian@gmail.com>
2026-06-08 17:43:40 +0600

    second task completed

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+let myName = 'Al Nahian';
+let age = 27;
+let isStudent = true;
+let salary = null;
+let phone;
+
+console.log(
+  typeof myName,
+  typeof age,
+  typeof isStudent,
+  typeof salary,
+  typeof phone,
+);
